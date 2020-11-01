c77f26be5a9e11ea9b6557761d8de3d12c39f942
matviiko <matviy.kopchak@gmail.com>
2020-11-01 10:56:25 +0200

    add build

---
+++ precache-manifest.8341d1f7dad400abacc6424c33097504.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e30a76ed8d50f63bf2160719a848e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99071d889455ac4d8252",
+    "url": "/static/css/main.c220a4c4.chunk.css"
+  },
+  {
+    "revision": "9eb725eb9b407121bb0f",
+    "url": "/static/js/2.629e2dbc.chunk.js"
+  },
+  {
+    "revision": "ed901023237d66f16963aae8c7889084",
+    "url": "/static/js/2.629e2dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99071d889455ac4d8252",
+    "url": "/static/js/main.af9c7270.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "022b7289305ce125814b8b5640c0f1ec",
+    "url": "/static/media/matvii_kopchak.022b7289.png"
+  }
+]);
